a0def34b74e8ef7399c260046461c09218efb4d1
adrenos660 <adrenos660@gmail.com>
2025-12-29 19:16:45 +0100

    Update GitHub repository URL in autoupdate script

--- wax/payloads/autoupdate.sh
+++ wax/payloads/autoupdate.sh
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 
-url="https://api.github.com/repos/MercuryWorkshop/sh1mmer"
+url="https://api.github.com/repos/adrenos660/gh-actions-sh1mmer"
 content_url="${url}/contents/wax/payloads?ref=beautifulworld"
 
 if ! command -v jq &> /dev/null; then
